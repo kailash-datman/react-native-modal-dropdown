ec9ac409415918db26df96d3fc5e41e06ce64a42
kailash-datman <71750611+kailash-datman@users.noreply.github.com>
2021-08-13 14:53:04 +0530

    "Updated Flatlist component"

--- components/ModalDropdown.js
+++ components/ModalDropdown.js
@@ -431,7 +431,9 @@ export default class ModalDropdown extends Component {
     return (
       <FlatList
         {...dropdownListProps}
+        getItemLayout={(data, index) => { return {length: 33 + StyleSheet.hairlineWidth, index, offset: (33 + StyleSheet.hairlineWidth) * index} }}
         data={options}
+        ref={component => (this.flatList = component)}
         scrollEnabled={scrollEnabled}
         initialScrollIndex={saveScrollPosition ? selectedIndex : -1}
         style={styles.list}
@@ -442,6 +444,12 @@ export default class ModalDropdown extends Component {
         showsVerticalScrollIndicator={showsVerticalScrollIndicator}
         keyboardShouldPersistTaps={keyboardShouldPersistTaps}
         ListHeaderComponent={this._renderSearchInput}
+        onScrollToIndexFailed={info => {
+          const wait = new Promise(resolve => setTimeout(resolve, 500));
+          wait.then(() => {
+            this.flatList.scrollToIndex({ index: info.index, animated: true });
+          });
+        }}
       />
     );
   }
